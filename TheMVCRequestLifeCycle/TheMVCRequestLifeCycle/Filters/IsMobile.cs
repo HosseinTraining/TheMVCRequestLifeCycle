7d2a537fddfd23ff4b6eb4acfff4d6b3f1bc804f
Hossein Alizadeh <hosseinalizadeh@hotmail.com>
2018-02-03 15:44:11 +0330

    AddIsMobileActionMethodSelectorAttribute

---
+++ TheMVCRequestLifeCycle/TheMVCRequestLifeCycle/Filters/IsMobile.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace TheMVCRequestLifeCycle.Filters
+{
+    public class IsMobile:ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return controllerContext.HttpContext.Request.Browser.IsMobileDevice;
+        }
+    }
+}
